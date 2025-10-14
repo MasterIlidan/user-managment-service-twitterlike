9748f99c092db686d1f39bf1df51795b4d86e796
MasterIlidan <segamil2001@gmail.com>
2025-10-14 14:25:40 +0500

    Исправил очепятку

--- src/main/java/com/masterilidan/usermanagmentservicetwitterlike/UserManagementServiceTwitterlikeApplication.java
+++ src/main/java/com/masterilidan/usermanagmentservicetwitterlike/UserManagementServiceTwitterlikeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserManagmentServiceTwitterlikeApplication {
+public class UserManagementServiceTwitterlikeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserManagmentServiceTwitterlikeApplication.class, args);
+        SpringApplication.run(UserManagementServiceTwitterlikeApplication.class, args);
     }
 
 }
